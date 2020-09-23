1d2f142d3c0bbd8186507cc3ee1ea04712aeb961
marcolerco <marcolerco@gmail.com>
2020-09-23 10:30:38 +0200

    python 3 fix in import.

--- django_skebby/urls.py
+++ django_skebby/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from views import credit_left
+from .views import credit_left
 
 urlpatterns = [
     url(r'credit_left/$', credit_left, {}, 'skebby-credit-left'),
